31e571691f35170796b768b58e17b6c21e6bb410
yumeminami <shikinamiasuka1103@gmail.com>
2023-04-27 16:27:19 +0800

    fix ModuleNotFoundError

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
-from app.main import create_app
 import pytest
 from fastapi.testclient import TestClient
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ["ENV"] = "test"
+from app.main import create_app
 
 
 @pytest.fixture(scope="module")

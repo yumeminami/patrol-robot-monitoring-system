474580ff08a3f9bfb7847678baa20699980e16b0
yumeminami <shikinamiasuka1103@gmail.com>
2023-05-08 17:27:47 +0800

    add task enum and validator

--- app/schemas/tasks.py
+++ app/schemas/tasks.py
@@ -3,7 +3,17 @@ from typing import List
 from datetime import time
 from .sensors import SensorForTask
 from .vision_algorithms import VisionAlgorithmForTask
+from enum import Enum
 
+class TaskType(Enum):
+    AUTO = 0
+    MANUAL = 1
+
+class TaskStatus(Enum):
+    NOT_STARTED = 0
+    IN_PROGRESS = 1
+    PENDING = 2
+    COMPLETED = 3
 
 class TaskBase(BaseModel):
     type: int
@@ -19,21 +29,21 @@ class TaskBase(BaseModel):
     is_everyday: bool = False
 
     @validator("type")
-    def type_must_be_in_range(cls, v):
+    def type_validator(cls, v):
         if v not in range(0, 2):
-            raise ValueError("type must be maunal(0) or auto(1)")
+            raise ValueError("type must be auto(0) or maunal(1)")
         return v
 
     @validator("status")
-    def status_must_be_in_range(cls, v):
+    def status_validator(cls, v):
         if v not in range(0, 4):
             raise ValueError(
-                "status must be not started(0), in progress(1), paused(2), or completed(3)"
+                "status must be not started(0), in progress(1), pending(2) or completed(3)"
             )
         return v
 
     @validator("sensors")
-    def sensors_must_belong_to_robot(cls, v, values):
+    def sensors_validator(cls, v, values):
         if v:
             for sensor in v:
                 if sensor.robot_id != values["robot_id"]:

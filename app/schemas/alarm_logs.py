734fa14f79c8774ac8b6bab7c9ecd550c67c02f1
yumeminami <shikinamiasuka1103@gmail.com>
2023-05-08 14:00:31 +0800

    add alarmlog enum and validator

--- app/schemas/alarm_logs.py
+++ app/schemas/alarm_logs.py
@@ -1,6 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
 from datetime import datetime
+from enum import Enum
+
+
+class AlarmLogLevel(Enum):
+    WARNING = 0
+    FATAL = 1
+
+
+class AlarmLogStatus(Enum):
+    UNPROCESSED = 0
+    PROCESSED = 1
+
+
+class AlarmLogType(Enum):
+    DEVICE = 0
+    TEMPERATURE = 1
+    HUMIDITY = 2
 
 
 class AlarmLogBase(BaseModel):
@@ -11,6 +28,12 @@ class AlarmLogBase(BaseModel):
     location: int
     img_url: Optional[str] = None
     video_url: Optional[str] = None
+
+    @validator("status")
+    def status_validator(cls, v):
+        if v not in range(0, 2):
+            raise ValueError("status must be unprocessed(0) or processed(1)")
+        return v
 
 
 class AlarmLogCreate(AlarmLogBase):

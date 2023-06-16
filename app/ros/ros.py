0de0d3e3ffb4d81b76c20cbfc787570ba0e7e7a5
yumeminami <shikinamiasuka1103@gmail.com>
2023-06-16 14:06:41 +0800

    implement update data to redis in subcriber callback

--- app/ros/ros.py
+++ app/ros/ros.py
@@ -7,6 +7,7 @@ import rospy
 
 from app.utils.log import logger, log_queue
 from app.db.database import SessionLocal
+from app.db.redis import redis_client
 from app.crud.robots import robot as crud
 from app.schemas.robots import Robot
 
@@ -64,15 +65,15 @@ class Node:
         """
 
         db = SessionLocal()
-        robotname = rospy.get_name().strip("/").replace("_subscriber", "")
+        robot_name = rospy.get_name().strip("/").replace("_subscriber", "")
 
         for topic in topic_list:
-            topic = "/{robotname}/{topic}".format(
-                robotname=robotname, topic=topic
+            topic = "/{robot_name}/{topic}".format(
+                robot_name=robot_name, topic=topic
             )
-            self.wait_for_topic(robotname, topic)
+            self.wait_for_topic(robot_name, topic)
 
-    def wait_for_topic(self, robotname, topic):
+    def wait_for_topic(self, robot_name, topic):
         """Ensures a topic is available before creating a subscription.
 
         This function periodically checks if a given topic is being published. Once the topic
@@ -106,8 +107,12 @@ class Node:
 
             callback_method = getattr(self, f"{topic_type}_callback")
 
+            callback_args = {"robot_name": robot_name}
             self.subscribers[topic] = rospy.Subscriber(
-                name=topic, data_class=msg_class, callback=callback_method
+                name=topic,
+                data_class=msg_class,
+                callback=callback_method,
+                callback_args=callback_args,
             )
         except KeyError:
             logger.error(f"Topic type '{topic_type}' is not defined")
@@ -116,23 +121,63 @@ class Node:
                 f"Callback function for topic type '{topic_type}' is not defined"
             )
 
-    def robot_real_time_info_callback(self, message):
-        """Callback function for the subscriber
+    def robot_real_time_info_callback(self, message, args):
+        """Callback function for handling robot real-time info.
 
-        :param message: the message received from the topic
+        This function is called whenever a new message is published on the robot's real-time info topic.
+        It parses the message, updates the data in the cache, and logs the operation.
 
-        We will parse the message and update the data in the cache.
+        :param message: the ROS message received from the topic.
+        :param args: additional arguments passed to the callback (e.g., robot name).
         """
-        logger.info(f"Received message from topic: \n{message}")
+        try:
+            robot_name = args.get("robot_name")
+            if not robot_name:
+                logger.error("Robot name not found in callback arguments.")
+                return
 
-    def sensor_data_callback(self, message):
-        """Callback function for the subscriber
+            info = {
+                field: message.__getattribute__(field)
+                for field in message.__slots__
+            }
 
-        :param message: the message received from the topic
+            redis_client.hset(robot_name, "robot_real_time_info", str(info))
 
-        We will parse the message and update the data in the cache.
+            # logger.info(f"Received message from topic: \n{message}")
+            logger.info(f"Updated real-time info for robot: {robot_name}")
+
+        except Exception as e:
+            logger.error(
+                f"Error occurred while processing real-time info: {e}"
+            )
+
+    def sensor_data_callback(self, message, args):
+        """Callback function for handling sensor data.
+
+        This function is called whenever a new message is published on the robot's sensor data topic.
+        It parses the message, updates the data in the cache, and logs the operation.
+
+        :param message: the ROS message received from the topic.
+        :param args: additional arguments passed to the callback (e.g., robot name).
         """
-        logger.info(f"Received message from topic: \n{message}")
+        try:
+            robot_name = args.get("robot_name")
+            if not robot_name:
+                logger.error("Robot name not found in callback arguments.")
+                return
+
+            info = {
+                field: message.__getattribute__(field)
+                for field in message.__slots__
+            }
+
+            redis_client.hset(robot_name, "sensor_data", str(info))
+
+            # logger.info(f"Received message from topic: \n{message}")
+            logger.info(f"Updated sensor data for robot: {robot_name}")
+
+        except Exception as e:
+            logger.error(f"Error occurred while processing sensor data: {e}")
 
 
 def initialize_all_robots_corresponding_nodes():
@@ -178,7 +223,7 @@ def run_node(robotname, ros_port_queue):
         return
     xmlrpc_port = ros_port_queue.get()
     tcpros_port = ros_port_queue.get()
-    nodename = robotname + "_subscriber"
+    nodename = f"{robotname}_subscriber"
     logger.debug(f"Creating node: {nodename}")
     logger.debug(f"xmlrpc_port: {xmlrpc_port}")
     logger.debug(f"tcpros_port: {tcpros_port}")

270d2af57c2b7f55e8a3647e836c96e4413d602a
yumeminami <shikinamiasuka1103@gmail.com>
2023-06-28 16:20:40 +0800

    fix/stop the task execution while task has been removed

--- app/celery_app/celery.py
+++ app/celery_app/celery.py
@@ -32,6 +32,9 @@ def start_task(task_id, eta_time):
     db = SessionLocal()
 
     task = crud.get(db, task_id)
+    if task is None:
+        # If the is_everyday is True, we need to stop the task execution
+        return "task not found"
     crud.update(
         db, db_obj=task, obj_in={"status": TaskStatus.IN_PROGRESS.value}
     )

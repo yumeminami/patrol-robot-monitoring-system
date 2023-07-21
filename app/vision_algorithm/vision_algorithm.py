ec6d188eb8bc321a39e8ab3ae25dc233d8b4b1e0
yumeminami <shikinamiasuka1103@gmail.com>
2023-07-21 10:13:07 +0800

    feature/implement vision algorithm detect and merge

---
+++ app/vision_algorithm/vision_algorithm.py
@@ -0,0 +1,77 @@
+import base64
+import json
+import os
+from uuid import uuid4
+
+import cv2
+import requests
+
+from app.utils.log import logger
+
+CONFIG_PATH = "app/vision_algorithm/config.json"
+
+
+class VisionAlgorithm:
+    def __init__(self):
+        with open(CONFIG_PATH, "r") as f:
+            self.config = json.load(f)
+
+    def detect(self, image_id: str, image_base64: str, algorithm: str, sensitivity: float):
+        vision_algorithm_api_url = os.environ.get("VISION_ALGORITHM_API_URL")
+        if vision_algorithm_api_url is None:
+            raise Exception("VISION_ALGORITHM_API_URL is not set")
+
+        url = f"{vision_algorithm_api_url}/{algorithm}/"
+
+        params = {
+            "img_id": image_id,
+            "image_base": image_base64,
+            "sensitivity": sensitivity,
+        }
+
+        try:
+            response = requests.get(url, json=params)
+            receive_data = response.json()
+        except Exception as e:
+            logger.error(f"vision_algorithm detect error: {e}")
+            return None
+        print(receive_data["data"]["counts"])
+        counts = receive_data["data"]["counts"]
+        alarms = self.config[algorithm]["alarms"]
+        detected_alarm = []
+
+        try:
+            for alarm in alarms:
+                if counts[alarm] > 0:
+                    detected_alarm.append(alarm)
+        except Exception as e:
+            logger.error(f"vision_algorithm detect error: {e}")
+        finally:
+            return detected_alarm
+
+    def merge(self, image_id, image_base64):
+        vision_algorithm_api_url = os.environ.get("VISION_ALGORITHM_API_URL")
+        if vision_algorithm_api_url is None:
+            raise Exception("VISION_ALGORITHM_API_URL is not set")
+
+        url = f"{vision_algorithm_api_url}/merge/"
+        params = {
+            "img_id": image_id,
+            "image_base": image_base64,
+        }
+        image_data = None
+        try:
+            response = requests.get(url, json=params)
+            image_data = response.json()["data"]["img"]
+            print(response.json().keys())
+            if image_data is None:
+                logger.error("merge image data is None")
+        except KeyError as e:
+            logger.error(f"image data is None: {e}")
+        except Exception as e:
+            logger.error(f"vision_algorithm merge error: {e}")
+        finally:
+            return image_data
+
+
+vision_algorithm = VisionAlgorithm()
---
+++ tests/vision_algorithm_test/vision_alogorithm_demo.py
@@ -0,0 +1,51 @@
+from app.vision_algorithm.vision_algorithm import vision_algorithm as vs
+import os
+import base64
+import argparse
+from uuid import uuid4
+
+# PLEASE SET PYTHONPATH MANUALLY BEFORE RUN THIS SCRIPT
+# export PYTHONPATH=~/patrol-robot-monitoring-system
+os.environ.setdefault(
+    "PYTHONPATH", "/home/ubuntu/patrol-robot-monitoring-system")
+os.environ.setdefault("VISION_ALGORITHM_API_URL", "http://192.168.2.41:5209")
+print(os.environ.get("PYTHONPATH"))
+print(os.environ.get("VISION_ALGORITHM_API_URL"))
+
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "-s", "--sensitivity", type=float, default=0.1, help="sensitivity"
+)
+
+ap.add_argument(
+    "-a", "--algorithm", type=str, required=True, help="algorithm"
+)
+
+ap.add_argument(
+    "-i", "--image", type=str, required=True, help="image file path"
+)
+args = vars(ap.parse_args())
+
+# due to the algorithm amount is much, we don't check is it exist
+if __name__ == "__main__":
+
+    image_id = str(uuid4())
+    if not os.path.exists(args["image"]):
+        print("image file not exist")
+        exit(1)
+    with open(args["image"], "rb") as f:
+        image_data = f.read()
+    image_base64 = base64.b64encode(image_data).decode()
+    print(vs.detect(image_id, image_base64,
+          args["algorithm"], args["sensitivity"]))
+
+    merge_image_base64 = vs.merge(image_id, image_base64)
+    if merge_image_base64 is None:
+        print("merge image error")
+        exit(1)
+    merge_image_data = base64.b64decode(merge_image_base64)
+    with open(
+        f"app/static/images/{image_id}.jpg",
+        "wb",
+    ) as f:
+        f.write(merge_image_data)

3297098f3143812fec9d1c0f24e2d6629ba50132
yumeminami <shikinamiasuka1103@gmail.com>
2023-08-17 16:26:56 +0800

    feature/delete image file when delete patrol image record

--- app/api/endpoints/patrol_images.py
+++ app/api/endpoints/patrol_images.py
@@ -1,4 +1,4 @@
-from app.api.api import create_generic_router
+from app.api.api import create_generic_router, remove_file
 from app.crud.patrol_images import patrol_image as crud
 from app.schemas.patrol_images import (
     PatrolImage,
@@ -6,6 +6,15 @@ from app.schemas.patrol_images import (
     PatrolImageUpdate,
 )
 
+
+def after_delete(id, patrol_image, db):
+    remove_file("app/"+patrol_image.image_url)
+
+
+patrol_images_hooks = {
+    "after_delete": after_delete,
+}
+
 router = create_generic_router(
-    crud, PatrolImageCreate, PatrolImageUpdate, PatrolImage
+    crud, PatrolImageCreate, PatrolImageUpdate, PatrolImage, hooks=patrol_images_hooks
 )

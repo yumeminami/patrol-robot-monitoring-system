3ab6c158b192fd0075913892032c1ffcc2fcced2
yumeminami <shikinamiasuka1103@gmail.com>
2023-08-24 15:01:48 +0800

    update/remove gimbal_point create and update api

--- app/api/endpoints/gimbalpoints.py
+++ app/api/endpoints/gimbalpoints.py
@@ -2,10 +2,8 @@ from app.api.api import create_generic_router
 from app.crud.gimbalpoints import gimbal_point as crud
 from app.schemas.gimbalpoints import (
     GimbalPoint,
-    GimbalPointCreate,
-    GimbalPointUpdate,
 )
 
 router = create_generic_router(
-    crud, GimbalPointCreate, GimbalPointUpdate, GimbalPoint
+    crud=crud, create_schema=None, update_schema=None, db_model=GimbalPoint
 )

f80b1e7c8e052c7964bcbe7a10dc55518e6114ff
yumeminami <shikinamiasuka1103@gmail.com>
2023-09-04 14:07:30 +0800

    feature/add task_logs zh and exporter api

--- app/api/exporter.py
+++ app/api/exporter.py
@@ -33,6 +33,32 @@ async def export_tasks():
         raise HTTPException(status_code=400, detail="Unable to fetch tasks")
 
 
+@router.get("/task_logs")
+async def export_tasks():
+    url = "http://localhost:8000/api/zh/task_logs"
+    headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyb290IiwiZXhwIjoxNjg1MTI1MjIyfQ.Hkt-ptLsKkRrZ8UdT6AoVAf0gPaUuHA24OjqDH4QzRc"
+    }
+
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url, headers=headers)
+
+    if r.status_code == 200:
+        file_name = "task_logs.xlsx"
+
+        data = r.json()
+        write_excel(data, file_name)
+
+        return FileResponse(
+            file_name,
+            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            filename=file_name,
+        )
+
+    else:
+        raise HTTPException(status_code=400, detail="Unable to fetch tasks")
+
+
 @router.get("/robot")
 async def export_robots():
     url = "http://localhost:8000/api/zh/robots"

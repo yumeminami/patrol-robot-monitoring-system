db040e721255993bcbf8855c0c05cab96407d070
yumeminami <shikinamiasuka1103@gmail.com>
2023-12-19 09:35:29 +0800

    update/update alarm log type

--- app/api/endpoints/alarm_logs.py
+++ app/api/endpoints/alarm_logs.py
@@ -1,9 +1,11 @@
 from app.api.api import create_generic_router
 from app.crud.alarm_logs import alarm_log as crud
-from app.schemas.alarm_logs import AlarmLog, AlarmLogUpdate
+from app.schemas.alarm_logs import AlarmLog, AlarmLogUpdate, alarm_log_type_map
 
 
 def after_reads(alarm_logs):
+    for alarm_log in alarm_logs:
+        alarm_log.type = alarm_log_type_map.get(alarm_log.type, alarm_log.type)
     return alarm_logs[::-1]
 
 

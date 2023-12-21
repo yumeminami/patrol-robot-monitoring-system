cf8c6b9f302b820db4f10783907dd21782ce40b4
yumeminami <shikinamiasuka1103@gmail.com>
2023-12-21 18:41:35 +0800

    update/Update database engine configuration

--- app/db/database.py
+++ app/db/database.py
@@ -8,7 +8,7 @@ from app.utils.log import logger
 SQLALCHEMY_DATABASE_URL = config.DATABASE_URL
 
 logger.info("Connecting to database...")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,pool_size=100, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 logger.info("Connected to database!")
 
